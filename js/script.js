c2f3d179e3a46462e3eae90a4a5d6e38ceefe840
Damjan15 <stamenovdamjan20@gmail.com>
2023-02-13 09:35:22 +0100

    function for fetching data from an API created, displaying popular movies

--- js/script.js
+++ js/script.js
@@ -1,44 +1,93 @@
 const global = {
-    currentPage: window.location.pathname
-}
+  currentPage: window.location.pathname,
+};
 
 // Highlight active link
 function highlightActiveLink() {
-    const links = document.querySelectorAll(".nav-link");
+  const links = document.querySelectorAll(".nav-link");
 
-    links.forEach((link) => {
-        if (link.getAttribute('href') === global.currentPage) {
-            link.classList.add('active');
-        }
-    })
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Fetch data from TMDB Api
+async function fetchAPIData(endpoint) {
+  const API_KEY = "e8de5f160422af13da9d5bb66932bd91";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+
+  return data;
+}
+
+// Get Popular Movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="Movie Title"
+          />`
+              : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="Movie Title"
+        />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>
+    `;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+
 }
 
 // Init App
 function init() {
-    switch (global.currentPage) {
-        case "/":
-        case "/index.html":
-            console.log("Homepage")
-            break;
-        case "/shows.html":
-            console.log("Shows")
-            break;
-        case "/movie-details.html":
-            console.log("Movie Details")
-            break;
-        case "/tv-details.html":
-            console.log("TV Details")
-            break;
-        case "/search.html":
-            console.log("Search");
-            break;
-        default:
-            break;
-    }
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Homepage");
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+    default:
+      break;
+  }
 
-    highlightActiveLink();
+  highlightActiveLink();
 }
-
 
 // Event Listeners
 document.addEventListener("DOMContentLoaded", init);
