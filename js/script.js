77a1216c94469a098da3dd19b9184c08837acd80
Damjan15 <stamenovdamjan20@gmail.com>
2023-02-13 11:05:51 +0100

    movie details page created

--- js/script.js
+++ js/script.js
@@ -78,7 +78,6 @@ async function displayPopularMovies() {
 // Display 20 most popular TV Shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData("tv/popular");
-  console.log(results);
 
   results.forEach((show) => {
     const div = document.createElement("div");
@@ -102,13 +101,71 @@ async function displayPopularShows() {
             <div class="card-body">
               <h5 class="card-title">${show.name}</h5>
               <p class="card-text">
-                <small class="text-muted">Air Date: ${show.first_air_date}</small>
+                <small class="text-muted">Air Date: ${
+                  show.first_air_date
+                }</small>
               </p>
             </div>
         `;
 
     document.querySelector("#popular-shows").appendChild(div);
   });
+}
+
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="Movie Title"
+      />`
+          : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+      }
+    </div>
+    <div>
+      <h2>${ movie.title }</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${ movie.vote_average.toFixed(1) } / 10
+      </p>
+      <p class="text-muted">Release Date: ${ movie.release_date }</p>
+      <p>
+        ${ movie.overview }
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${ movie.genres.map((genre) => `<li>${ genre.name }</li>`).join("")}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${ movie.budget }</li>
+      <li><span class="text-secondary">Revenue:</span> $${ movie.revenue }</li>
+      <li><span class="text-secondary">Runtime:</span> ${ movie.runtime } minutes</li>
+      <li><span class="text-secondary">Status:</span> ${ movie.status }</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${ movie.production_companies.map(( company ) => `<span>${ company.name }</span>`).join(' ')}</div>
+  </div>
+    `;
+
+    document.querySelector('#movie-details').appendChild(div);
 }
 
 // Init App
@@ -125,6 +182,7 @@ function init() {
       break;
     case "/movie-details.html":
       console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
