7bf72b4d0bbb104f201fb15c7c4e5b12cd5cc845
Damjan15 <stamenovdamjan20@gmail.com>
2023-02-13 15:30:04 +0100

    pagination added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const global = {
   search: {
     term: "",
     type: "",
-    pagination: 1,
+    page: 1,
     totalPages: 1,
+    totalResults: 0
   },
   api: {
     apiKey: "e8de5f160422af13da9d5bb66932bd91",
@@ -47,7 +48,7 @@ async function searchAPIData() {
 
   showSpinner();
 
-  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
   const data = await response.json();
 
   hideSpinner();
@@ -314,8 +315,11 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if (global.search.term !== "" && global.search.term !== null) {
-    // @todo - make request and display results
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
     
     if (results.length === 0) {
         showAlert("No results found");
@@ -333,7 +337,6 @@ async function search() {
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
-  console.log(results);
 
   results.forEach((movie) => {
     const div = document.createElement("div");
@@ -356,6 +359,11 @@ async function displaySlider() {
 
 // Display Search Results
 function displaySearchResults(results) {
+    // Clear previous results
+    document.querySelector("#search-results").innerHTML = "";
+    document.querySelector("#search-results-heading").innerHTML = "";
+    document.querySelector("#pagination").innerHTML = "";
+
     results.forEach((result) => {
         const div = document.createElement("div");
         div.classList.add("card");
@@ -382,9 +390,52 @@ function displaySearchResults(results) {
               </p>
             </div>
         `;
+
+        document.querySelector("#search-results-heading").innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+        `;
     
         document.querySelector("#search-results").appendChild(div);
       });
+
+    displayPagination();
+}
+
+// Display pagination for search
+function displayPagination() {
+    const div = document.createElement("div");
+    div.classList.add("pagination");
+    div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+    document.querySelector("#pagination").appendChild(div);
+
+    // Disable prev button if on first page
+    if (global.search.page === 1) {
+        document.querySelector("#prev").disabled = true;
+    }
+
+    // Disable next button if on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector("#next").disabled = true;
+    }
+
+    // Next page
+    document.querySelector("#next").addEventListener("click", async () => {
+        global.search.page++;
+        const { results } = await searchAPIData();
+        displaySearchResults(results); 
+    });
+
+    // Prev page
+    document.querySelector("#prev").addEventListener("click", async () => {
+        global.search.page--;
+        const { results } = await searchAPIData();
+        displaySearchResults(results); 
+    });
 }
 
 // Initialize Swiper
