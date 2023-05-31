07fa9c9cbaec743361e196a515c2ba5701f32b86
Damjan15 <stamenovdamjan20@gmail.com>
2023-05-31 20:58:00 +0200

    simple router added, highlighting active links in navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-console.log(import.meta.env.VITE_MOVIE_DB_API_KEY);
+const global = {
+    currentPage: window.location.pathname
+}
+
+// Initialize Application (Routes)
+function init() {
+    switch (global.currentPage) {
+        case "/":
+        case "/index.html":
+            console.log("Homepage - Movies");
+            break;
+        case "/shows.html":
+            console.log("Shows");
+            break;
+        case "/movie-details.html":
+            console.log("Movies Details");
+            break;
+        case "/tv-details.html":
+            console.log("TV Details");
+            break;
+        case "/search.html":
+            console.log("Search Page");
+            break;
+        default:
+            break;
+    }
+
+    highlightActiveLink();
+}
+
+// Highlight Active Link
+function highlightActiveLink() {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((link) => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", init);
