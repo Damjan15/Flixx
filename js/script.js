8904e9dd0cfca1876d8b89ad9ce3c03d17688f37
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-20 10:26:44 +0200

    feat: showing spinner when fetching data & fetching popular tv shows

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function init() {
       displayPopularMovies();
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayPopularShows();
       break;
     case "/movie-details.html":
       console.log("Movie Details");
@@ -40,10 +40,14 @@ async function fetchAPIData(endpoint) {
   const API_KEY = "d05e14453e201026de378997b91fa8cf";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
+
+  hideSpinner();
 
   return data;
 }
@@ -80,6 +84,47 @@ async function displayPopularMovies() {
 
     document.querySelector("#popular-movies").appendChild(div);
   });
+}
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `<a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 document.addEventListener("DOMContentLoaded", init);
