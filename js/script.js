38fe3b74c84fb0147e94f0f948ab07d334a543cb
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-20 12:52:37 +0200

    feat: displaying search results

--- js/script.js
+++ js/script.js
@@ -325,20 +325,71 @@ async function displaySlider() {
   });
 }
 
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+        <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${
+                global.search.type === "movie" ? result.title : result.name
+              }"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+             alt="${
+               global.search.type === "movie" ? result.title : result.name
+             }"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${
+              global.search.type === "movie" ? result.title : result.name
+            }</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${
+                global.search.type === "movie"
+                  ? result.release_date
+                  : result.first_air_date
+              }</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector("#search-results").appendChild(div);
+  });
+}
+
 async function search() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
-    // Add the type and term to the global object
-    global.search.type = urlParams.get('type');
-    global.search.term = urlParams.get('search-term');
+  // Add the type and term to the global object
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
 
-    if (global.search.term !== '' && global.search.term !== null) {
-        const results = await searchAPIData();
-        console.log(results);
-    } else {
-        showAlert('Please enter a search term');
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No results found");
+      return;
     }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
+  } else {
+    showAlert("Please enter a search term");
+  }
 }
 
 function showSpinner() {
@@ -376,13 +427,13 @@ function displayBackgroundImage(type, backgroundPath) {
 }
 
 // Show Alert
-function showAlert(message, className = 'error') {
-    const alertEl = document.createElement('div');
-    alertEl.classList.add('alert', className);
-    alertEl.appendChild(document.createTextNode(message));
-    document.querySelector('#alert').appendChild(alertEl);
+function showAlert(message, className = "error") {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
 
-    setTimeout(() => alertEl.remove(), 3000);
+  setTimeout(() => alertEl.remove(), 3000);
 }
 
 document.addEventListener("DOMContentLoaded", init);
