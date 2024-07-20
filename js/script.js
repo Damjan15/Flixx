99a04da83e7a11d56fc30306ae7044a15ccd8ac8
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-20 12:45:26 +0200

    feat: search functionality created, showing alert functionality created

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: "d05e14453e201026de378997b91fa8cf",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 // Highlight Active Link
@@ -30,7 +40,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
 
@@ -62,14 +72,31 @@ function initSwiper() {
 }
 
 async function fetchAPIData(endpoint) {
-  const API_KEY = "d05e14453e201026de378997b91fa8cf";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
   const data = await response.json();
 
   hideSpinner();
@@ -298,6 +325,22 @@ async function displaySlider() {
   });
 }
 
+async function search() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    // Add the type and term to the global object
+    global.search.type = urlParams.get('type');
+    global.search.term = urlParams.get('search-term');
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        const results = await searchAPIData();
+        console.log(results);
+    } else {
+        showAlert('Please enter a search term');
+    }
+}
+
 function showSpinner() {
   document.querySelector(".spinner").classList.add("show");
 }
@@ -330,6 +373,16 @@ function displayBackgroundImage(type, backgroundPath) {
   } else {
     document.querySelector("#show-details").appendChild(overlayDiv);
   }
+}
+
+// Show Alert
+function showAlert(message, className = 'error') {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+
+    setTimeout(() => alertEl.remove(), 3000);
 }
 
 document.addEventListener("DOMContentLoaded", init);
